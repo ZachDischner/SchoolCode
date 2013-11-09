1dcbe09fefff7e85045eba44efcbc8091efcb5ec
Zach Dischner <zach.dischner@colorado.edu>
2013-11-09 12:08:01 -0700

    GPS Working copy, Homework 8

--- ASEN5090-GPS/Homework/8/Homework8_main.m
+++ ASEN5090-GPS/Homework/8/Homework8_main.m
@@ -24,15 +24,16 @@ screen_size = get(0,'ScreenSize');
 sw = screen_size(3);    % Screen Width
 sh = screen_size(4);    % Screen Height
 addpath HW8_files
-soln_format = '|  %2.0f | %15.3f | %7.3f | %12.3f | %15.3f | %9.3f | %3.2f | %3.2f  \t\n';
+soln_format = '|  %2.0f | %15.3f | %7.3f | %12.3f | %6.3f | %9.3f | %3.2f | \t\n';
 % h = waitbar(0,'GO PARTY, ILL STAY HERE WORKING!!!');
 wb_tot = 1024+1238;
 tot_iters = 0;
+debug = 1;
 
 
 %% Setup Problem
 %------Define Navigation and Observation File
-RINEX_FILES = {'onsa2640.onehour','joze2640.onehour'};
+RINEX_FILES = {'joze2640.onehour','onsa2640.onehour'};
 nav_msg  = 'brdc2640.12n';
 
 %------Define Orbit determination parameters
@@ -44,9 +45,12 @@ params.c = 299792458; % [m/s]
 
 params.options=optimset('Display','off','TolFun',1e-10,'TolX',1e-10);
 
+%------Elevation Mask
+el_mask = 10; %degrees
+
 
 %------Define Zenith Correction for each site
-Tzenith = [2.3858, 2.4086]; %[m]
+Tzenith = [2.4086, 2.3858]; %[m]
 
 
 %------Read navigation message content
@@ -87,6 +91,10 @@ for file_idx=1:length(RINEX_FILES)
     [ fid, rec_xyz, observables ] = read_rinex_header( rinex_file );
     [rec_lla] = ecef2lla(rec_xyz');
     
+    %------Form least squares like variables
+    xr = rec_xyz(1); yr = rec_xyz(2); zr = rec_xyz(3);
+    X = [xr, yr, zr, 1]; 
+    
     %------Read Observation file
     obs_data    = read_rinex_obs3(rinex_file);
     cols        = obs_data.col; % Structure of column index descriptions
@@ -98,12 +106,9 @@ for file_idx=1:length(RINEX_FILES)
     
     %------Only look at first epoch
     [GPSSecAryUn,secs_idx] = unique(obs_data.data(:,TOW_col));
-    t1 = secs_idx(1); t2 = secs_idx(2)-1;
-    secs_idx = secs_idx(1);
-    GPSSecAryUn = GPSSecAryUn(t1:t2);
-    PRNS = obs_data.data(t1:t2 , PRN_col);
-    GPSWeekAry = obs_data.data(t1:t2 , WEEK_col);
-    GPSSecAry = obs_data.data(t1:t2 , TOW_col);
+    PRNS = obs_data.data(: , PRN_col);
+    GPSWeekAry = obs_data.data(: , WEEK_col);
+    GPSSecAry = obs_data.data(: , TOW_col);
     %     GPS_Secs = obs_data.data(rows,SOW_col);
     %     GPS_Weeks = obs_data.data(rows,Week_col);
     
@@ -116,8 +121,9 @@ for file_idx=1:length(RINEX_FILES)
     res = rho_obs;
     ionFree = rho_obs;
     sat_prn = rho_obs;
-    iter = 0;
-    for sec_idx = secs_idx(1)'
+    epoch_iter = 0;
+    for sec_idx = secs_idx'
+        epoch_iter = epoch_iter + 1;
         
         %% Calculate "Modeled" Pseudorange
         %------Setup Range Finding
@@ -127,6 +133,7 @@ for file_idx=1:length(RINEX_FILES)
         
         %------Iterate over epoch satellite data
         Nsats = sum(GPSSecAry == GPSSecAry(sec_idx));
+        iter = 0; clear H y
         for sat = 1:Nsats
             data_idx = sec_idx+sat-1;
 %             waitbar((data_idx+tot_iters)/wb_tot,h)
@@ -135,10 +142,11 @@ for file_idx=1:length(RINEX_FILES)
             
             %------Calculate Geometric Range
             [R, rel_dt, satXYZ] = getSatGeomRange(rec_xyz', GPS_Week, GPS_SOW, PRN, nav_data, params);
+            xs = satXYZ(1); ys = satXYZ(2); zs = satXYZ(3);
             
             %------Check Elevation Angle
             [az, el(iter), r] = ecef2azelrange(satXYZ', rec_xyz);
-            if el(iter) < 10
+            if el(iter) < el_mask
                 iter=iter-1;
                 tot_iters = tot_iters+1;
                 continue
@@ -168,22 +176,78 @@ for file_idx=1:length(RINEX_FILES)
                 PorC1 = obs_data.data(data_idx,C1_col);
 %                 rho_obs(iter) = C1;
             end
-            rho_obs(iter) = 2.5457*PorC1-1.5457*P2;
+            if P2==0
+                rho_obs(iter) = PorC1;
+            elseif PorC1==0
+                rho_obs(iter) = P2;
+            else 
+                rho_obs(iter) = 2.5457*PorC1-1.5457*P2;
+            end
+            
+            %------Populate Least Squares Variables
+            H(iter,:) = [(xr-xs)/R, (yr-ys)/R, (zr-zs)/R, 1]; %??? What about clock???
+            % same as (rec_xyz' - satXYZ)/R
+            y(iter) = rho_obs(iter) - rho_model(iter);
             
             sat_prn(iter) = PRN;
             
-%             if iter < secs_idx(2) && file_idx == 1
-%                 if sat == 1
-%                     fprintf('|_PRN_|___geomRange_____|___rel___|____satClk____|_______P3________|__Prefit___|___el___|__Trop___|\n')
-%                 end
-%                 fprintf(1,soln_format,PRN,...
-%                     R,rel_corr,sat_corr,ionFree(iter),...
-%                     ionFree(iter)-rho_model(iter),...
-%                     el(iter), Tropo_corr)
-%             end
+            if epoch_iter == 1 && file_idx == 1 && debug == 1
+                if sat == 1
+                    fprintf('|_PRN_|________P3_______|___Geom Range___|____satClk____|__rel__|__Tropo___|___prefit___|\n')
+                end
+                fprintf(1,soln_format,PRN,...
+                    rho_obs(iter), R,sat_corr,...
+                    rel_corr,Tropo_corr,y(iter))
+            end
             
         end     % End Satellite Iteration
+        %------Obtain least squares correction
+        del_x(epoch_iter,:) = transpose((H'*H)\H'*y');
+        P = inv(H'*H)*(0.5)^2;
+        
+        %------Rotate
+        rec_xyz_adj = repmat(rec_xyz', epoch_iter, 1) - del_x(:,1:3);
+        rec_lla_adj = ecef2lla(rec_xyz_adj);
+        lat_station =  rec_lla_adj(epoch_iter,1);
+        lon_station = rec_lla_adj(epoch_iter,2);
+        P(1:3,1:3) = rotateCovariance(P(1:3,1:3), lat_station,lon_station);
+        sigmas(epoch_iter,:) = sqrt(diag(P))';
+
+        key_epoch = 1;
+        if epoch_iter == key_epoch
+            fprintf('\n\n----------------------------%s-------------------------',rinex_file)
+            rec_xyz_adj = repmat(rec_xyz', epoch_iter, 1) - del_x(key_epoch,1:3);
+            rec_lla_adj = ecef2lla(rec_xyz_adj);
+            [de, dn, du] = ecef2enuv(del_x(key_epoch,1), ...
+                                     del_x(key_epoch,2), ...
+                                     del_x(key_epoch,3), ...
+                                     rec_lla_adj(key_epoch,1),rec_lla_adj(key_epoch,2));
+           fprintf('\nEast/Sig (m) %3.3f %3.3f\n',de,sigmas(epoch_iter,1)) 
+           fprintf('North/Sig (m) %3.3f %3.3f\n',dn,sigmas(epoch_iter,2)) 
+           fprintf('Up/Sig (m) %3.3f %3.3f\n',du,sigmas(epoch_iter,3)) 
+           fprintf('Clk/Sig (m) %5.3f %3.3f\n',del_x(key_epoch,4),sigmas(epoch_iter,4))
+        end
     end     % End time iteration
+     
+
+    [de, dn, du] = ecef2enuv(del_x(:,1), ...
+                             del_x(:,2), ...
+                             del_x(:,3), ...
+                             rec_lla_adj(:,1),rec_lla_adj(:,2));
+%   or...
+    [xe,yn,zu]=ecef2enu(del_x(:,1),del_x(:,2),del_x(:,3),...
+                            rec_lla_adj(:,1),rec_lla_adj(:,2),rec_lla_adj(:,3),oblateSpheroid);
+    
+    t = linspace(GPSSecAryUn(1),GPSSecAryUn(end),length(GPSSecAryUn));
+    t = (t-t(1))/60;
+    figure; subplot(2,1,1);
+    plot(t,[de,dn,du])
+    xlabel('Minutes Since First Epoch');ylabel('Delta [m]'); legend('East','North','Up')
+    title(['Error for ', rinex_file])
+    subplot(2,1,2)
+    plot(t,sigmas(:,1:3))
+    xlabel('Minutes Since First Epoch');ylabel('STD'); legend('East','North','Up')
+    title(['Error for ', rinex_file])
     
 %     ecef2enuv()
     
@@ -193,7 +257,7 @@ for file_idx=1:length(RINEX_FILES)
     %% Plot
     
     
-    
+
 %     res = rho_obs-rho_model;
 % 
 %     
@@ -273,6 +337,7 @@ for file_idx=1:length(RINEX_FILES)
 %     end
 %     
 %     clear leg rows outliers sat_prn
+clear del_x H
     
 end     % End Rinex File Iteration
 
@@ -281,31 +346,31 @@ end     % End Rinex File Iteration
 figure_awesome
 
 
-%% SUPPORTING FUNCTION - Homework8_main.m
-type('Homework8_main.m')
-
-%% SUPPORTING FUNCTION - getSatGeomRange.m
-type('getSatGeomRange.m')
-
-%% SUPPORTING FUNCTION - date2GPSTime.m
-type('date2GPSTime.m')
-
-%% SUPPORTING FUNCTION - findNearestEphem.m
-type('findNearestEphem.m')
-
-%% SUPPORTING FUNCTION - calculateSatellitePosition.m
-type('calculateSatellitePosition.m')
-
-%% SUPPORTING FUNCTION - findFirstEpoch.m
-type('findFirstEpoch.m')
-
-%% SUPPORTING FUNCTION - getSatClockCorrection.m
-type('getSatClockCorrection.m')
-
-%% SUPPORTING FUNCTION - date2GPSTime.m
-type('GPSTime2Date.m')
-
-%% SUPPORTING FUNCTION - getTropoCorrection.m
-type('getTropoCorrection.m')
+% %% SUPPORTING FUNCTION - Homework8_main.m
+% type('Homework8_main.m')
+% 
+% %% SUPPORTING FUNCTION - getSatGeomRange.m
+% type('getSatGeomRange.m')
+% 
+% %% SUPPORTING FUNCTION - date2GPSTime.m
+% type('date2GPSTime.m')
+% 
+% %% SUPPORTING FUNCTION - findNearestEphem.m
+% type('findNearestEphem.m')
+% 
+% %% SUPPORTING FUNCTION - calculateSatellitePosition.m
+% type('calculateSatellitePosition.m')
+% 
+% %% SUPPORTING FUNCTION - findFirstEpoch.m
+% type('findFirstEpoch.m')
+% 
+% %% SUPPORTING FUNCTION - getSatClockCorrection.m
+% type('getSatClockCorrection.m')
+% 
+% %% SUPPORTING FUNCTION - date2GPSTime.m
+% type('GPSTime2Date.m')
+% 
+% %% SUPPORTING FUNCTION - getTropoCorrection.m
+% type('getTropoCorrection.m')
 
 fprintf('\nSim took %3.1f seconds to run\n',toc)
